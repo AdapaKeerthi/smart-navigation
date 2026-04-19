b1bc5761a0da0ffcbb47ce7c9ee5abdf540201b6
Your Name <your@email.com>
2026-04-19 19:19:34 +0530

    fix render port issue

--- app.py
+++ app.py
@@ -162,7 +162,10 @@ def dashboard():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port))
 @app.route('/admin')
 def admin_panel():
 
